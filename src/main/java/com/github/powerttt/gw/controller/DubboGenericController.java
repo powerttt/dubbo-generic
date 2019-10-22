813c5e99b51c8e55e3b8d55674bec893ee7a7b17
tn <powertn@126.com>
2019-10-22 09:56:33 +0800

    修改包名坐标

--- src/main/java/com/github/powerttt/gw/controller/DubboGenericController.java
+++ src/main/java/com/github/powerttt/gw/controller/DubboGenericController.java
@@ -1,13 +1,11 @@
 package com.github.powerttt.gw.controller;
 
-import com.changyuan.commons.ResultBean;
-import com.changyuan.commons.exception.UpExcetion;
+import com.github.powerttt.commons.ResultBean;
+import com.github.powerttt.commons.exception.UpExcetion;
 import com.github.powerttt.gw.config.Dubbo2RouterProperties;
 import com.github.powerttt.gw.entity.Dubbo2InterfaceProperties;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
